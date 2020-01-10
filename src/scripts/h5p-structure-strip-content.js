22067c3217faf8d54fc51942a9c8708dce610bd1
Oliver Tacke <o.tacke@posteo.de>
2020-01-10 11:03:07 +0100

    Remove slack for absolute text length minimum/maximum

--- src/scripts/h5p-structure-strip-content.js
+++ src/scripts/h5p-structure-strip-content.js
@@ -66,7 +66,7 @@ export default class StructureStripContent {
     this.mostImportantSegmentPercentage = this.mostImportantSegment.getWeight() / this.segments.reduce((previous, current) => previous + current.getWeight(), 0);
 
     // Maximum text length adjusted for weigth and slack mustn't be smaller that minimum text length
-    if (this.params.textLengthMax * this.mostImportantSegmentPercentage * (1 - this.params.slack / 100) < this.params.textLengthMin) {
+    if (this.params.textLengthMax * this.mostImportantSegmentPercentage < this.params.textLengthMin) {
       this.params.textLengthMax = Number.POSITIVE_INFINITY;
     }
 
@@ -143,12 +143,21 @@ export default class StructureStripContent {
    */
   buildFeedbackTexts(textTemplates) {
     let referenceLength = Math.max(this.mostImportantSegment.getText().length, this.mostImportantSegment.getWeight() / this.greatestCommonDivisor);
-    referenceLength = Math.max( referenceLength, this.params.textLengthMin * this.mostImportantSegmentPercentage);
-    referenceLength = Math.min( referenceLength, this.params.textLengthMax * this.mostImportantSegmentPercentage);
+
+    // Don't use slack for absolute text length minimum/maximum
+    let slackPercentage = this.params.slack / 100;
+    if (referenceLength < this.params.textLengthMin * this.mostImportantSegmentPercentage) {
+      referenceLength = this.params.textLengthMin * this.mostImportantSegmentPercentage;
+      slackPercentage = 0;
+    }
+    if (referenceLength > this.params.textLengthMax * this.mostImportantSegmentPercentage) {
+      referenceLength = this.params.textLengthMax * this.mostImportantSegmentPercentage;
+      slackPercentage = 0;
+    }
 
     const normalizedReferenceLength = referenceLength / this.mostImportantSegment.getWeight();
-    const normalizedLengthMax = normalizedReferenceLength * (1 + this.params.slack / 100);
-    const normalizedLengthMin = normalizedReferenceLength * (1 - this.params.slack / 100);
+    const normalizedLengthMax = normalizedReferenceLength * (1 + slackPercentage);
+    const normalizedLengthMin = normalizedReferenceLength * (1 - slackPercentage);
 
     const feedbackTexts = [];
     this.segments.forEach(segment => {
@@ -157,7 +166,7 @@ export default class StructureStripContent {
       if (normalizedLength > normalizedLengthMax) {
 
         // Too long compared to reference
-        const gap = Math.floor((normalizedLength - normalizedLengthMax) * segment.getWeight());
+        const gap = Math.round((normalizedLength - normalizedLengthMax) * segment.getWeight());
         if (gap === 0) {
           // Compensate for tiny text lengths
           feedbackTexts.push(null);
@@ -171,7 +180,7 @@ export default class StructureStripContent {
       else if (normalizedLength < normalizedLengthMin) {
 
         // To short compared to reference
-        const gap = Math.ceil((normalizedLengthMin - normalizedLength) * segment.getWeight());
+        const gap = Math.round((normalizedLengthMin - normalizedLength) * segment.getWeight());
         feedbackTexts.push(
           textTemplates.tooShort.replace(/@title/g, segment.getTitle()).replace(/@chars/g, gap)
         );
